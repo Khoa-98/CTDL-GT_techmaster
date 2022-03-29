efd737b77559167e5d3b07b4f9542356cfa74131
Khoa-98 <anhkhoa98nd@gmail.com>
2022-03-29 10:07:57 +0700

    bai 1295 tim sô các sô có chu so chan

---
+++ lesson_2/bai1295_findNumberwithEven.java
@@ -0,0 +1,38 @@
+/**
+ * bai1295_findNumberwithEven
+ */
+public class bai1295_findNumberwithEven {
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        //duyệt từng phần tử của mảng
+        for (int a : nums) {
+            // kiểm tra số chữ số của i
+            // nếu số chữ số mà chắn thì tăng count lên 1
+            int soLuongChuSo = tinhSoChuSo(a);
+            if (soLuongChuSo % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    //tinh so chu so cua 1 so 
+    private static int tinhSoChuSo(int a) {
+        int count = 0;
+        int result = a;
+
+        while (result != 0) {
+            result = a / 10;
+            a = result;
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] nums = { 1, 22, 333, 4444};
+
+        System.out.println(findNumbers(nums));
+    }
+}
+  
